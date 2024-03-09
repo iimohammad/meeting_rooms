716b5198951b6c2ecb9b457834792117e907312b
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-09 09:28:02 +0330

    created model.py for meeting_room

--- rooms/models.py
+++ rooms/models.py
@@ -11,3 +11,15 @@ class Team(models.Model):
 class company(models.Model):
     Teams = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
     company_name = models.CharField()
+
+class MeetingRoom(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    capacity = models.IntegerField()
+    is_booked = models.BooleanField(default=False)
+
+class Reservation(models.Model):
+    room = models.ForeignKey(MeetingRoom, on_delete=models.CASCADE)
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
