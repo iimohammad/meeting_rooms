93774cf95295b48c55a479e79f0ecc38a8988e2d
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-10 19:45:02 +0330

    created local_sitting.py

---
+++ config/local_sittings.py
@@ -0,0 +1,12 @@
+Secret_key = {
+    "secret_key" : "django-insecure-c^e+9+l%x4zx)s-s&kbi^lq7vdk0i9qwf^b8b3!=_bfk=lnl0b"
+}
+
+Email_Configuration = {
+    'EMAIL_BACKEND': 'django.core.mail.backends.smtp.EmailBackend',
+    'EMAIL_HOST': 'smtp.gmail.com',
+    'EMAIL_PORT': 587,
+    'EMAIL_HOST_USER': 'your-email@gmail.com',
+    'EMAIL_HOST_PASSWORD': 'your-password',
+    'EMAIL_USE_TLS': True
+}
