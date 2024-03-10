93014692c23b75f2e40a9c21ae8df37eed9b0077
masih1999-12 <msh77742@gmail.com>
2024-03-10 18:38:24 +0330

    modify urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path 
-from authentication.views import CustomLoginView , SignupView
+from django.urls import path
+from authentication.views import CustomLoginView, SignupView, profile_view, edit_profile_view, EmailLoginView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view() , name='login_view'),
-    path('signup/', SignupView.as_view(),name='signup_view'),
+    path('login/', CustomLoginView.as_view(), name='login_view'),
+    path('emaillogin/', EmailLoginView.as_view(), name='email_login_view'),
+    # path('login_phone/', CustomLoginView.as_view(), name='login_view'),
+    # path('login_github/', CustomLoginView.as_view(), name='login_view'),
+    # path('login_google/', CustomLoginView.as_view(), name='login_view'),
+    path('signup/', SignupView.as_view(), name='signup_view'),
+    path('profile/', profile_view, name='profile_view'),
+    path('editprofile/', edit_profile_view),
     # path('logout/', ),
-    # path('profile/', ),
-    # path('editprofile/', ),
-    
 ]
