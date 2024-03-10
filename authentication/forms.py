feeb9570a72a5d4005fbfa20e28d004078417723
masih1999-12 <msh77742@gmail.com>
2024-03-10 18:41:37 +0330

    modify authentication/forms.py

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,8 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from authentication.models import CustomUser
 
+
 class UserCreateForm(UserCreationForm):
     phone = forms.CharField(max_length=11)
+
     class Meta:
         model = CustomUser
-        fields = ('username', 'password1', 'password2', 'phone')
+        fields = ('username', 'email', 'first_name', 'last_name',
+                  'password1', 'password2', 'phone', 'profile_image')
