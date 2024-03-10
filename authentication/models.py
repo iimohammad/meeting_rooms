ce3bc503280d8db2a055d6c003fcd48c446c368d
masih1999-12 <msh77742@gmail.com>
2024-03-10 18:41:54 +0330

    add OTP model

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-        
+
+
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=11)
-    profile_image = models.ImageField(upload_to ='profile_images/')
+    profile_image = models.ImageField(upload_to='profile_images/')
+
+
+class OTPCode(models.Model):
+    email = models.CharField(max_length=255)
+    code = models.CharField(max_length=6)
