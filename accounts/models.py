c4589a50cf37c641d75b21e1fc144bb1a5589100
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-14 13:21:55 +0330

    models updated

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .validators import phone_validator
 
-
 class CustomUser(AbstractUser):
     class Gender(models.TextChoices):
         MALE = 'M', 'Male'
@@ -16,7 +15,15 @@ class CustomUser(AbstractUser):
     description = models.TextField(blank=True)
     profile_image = models.ImageField(upload_to='profile_images/')
 
-
 class OTPCode(models.Model):
     email = models.CharField(max_length=255)
     code = models.CharField(max_length=6)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+
+class Author(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='author_profile')
+    bio = models.TextField()
