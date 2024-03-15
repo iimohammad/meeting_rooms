04b98d82448307a3beb4e9cf6b4ccbdb7dd5b1fb
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-15 17:39:51 +0330

    json updated

--- rooms/models.py
+++ rooms/models.py
@@ -7,6 +7,14 @@ from django.utils.translation import gettext_lazy as _
 
 user = get_user_model()
 
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='teams')
+
+    def __str__(self):
+        return self.name
+
+
 class Company(models.Model):
     name = models.CharField(max_length=100)
 
