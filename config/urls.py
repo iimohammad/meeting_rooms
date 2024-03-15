842b3c32e084074cfa6aa3cf1bee5c3f26279350
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-03-15 18:06:52 +0330

    admin url change

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 from config import settings
+from config.local_settings import *
+from config.Sample_local_settings import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(Admin, admin.site.urls),  # For security Reasons, admin url can change
     path('auth/', include('accounts.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
     path('company/', include('company.urls')),
